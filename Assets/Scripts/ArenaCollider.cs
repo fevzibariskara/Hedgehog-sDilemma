30d61a5fc46b3794faff9c80c8dc4217c1513095
Fevzi Barış Kara <58820741+fevzibariskara@users.noreply.github.com>
2023-05-08 01:12:11 +0300

    .

--- Assets/Scripts/ArenaCollider.cs
+++ Assets/Scripts/ArenaCollider.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ArenaCollider : MonoBehaviour
 {
+    [SerializeField] int numSegments = 30; // Köþe sayýsý
+
     void Awake()
     {
         PolygonCollider2D poly = GetComponent<PolygonCollider2D>();
@@ -12,9 +14,49 @@ public class ArenaCollider : MonoBehaviour
             poly = gameObject.AddComponent<PolygonCollider2D>();
         }
 
-        Vector2[] points = poly.points;
-        EdgeCollider2D edge = gameObject.AddComponent<EdgeCollider2D>();
-        edge.points = points;
-        Destroy(poly);
+        Vector2[] points = CreateCirclePoints();
+        poly.points = points;
+
+        Destroy(GetComponent<EdgeCollider2D>());
+    }
+
+    Vector2[] CreateCirclePoints()
+    {
+        Vector2[] points = new Vector2[numSegments];
+        float angle = 0;
+        float angleDelta = 2 * Mathf.PI / numSegments;
+
+        for (int i = 0; i < numSegments; i++)
+        {
+            float x = Mathf.Cos(angle) * transform.localScale.x / 2;
+            float y = Mathf.Sin(angle) * transform.localScale.y / 2;
+            points[i] = new Vector2(x, y);
+            angle += angleDelta;
+        }
+
+        return points;
     }
 }
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ArenaCollider : MonoBehaviour
+//{
+//    void Awake()
+//    {
+//        PolygonCollider2D poly = GetComponent<PolygonCollider2D>();
+//        if (poly == null)
+//        {
+//            poly = gameObject.AddComponent<PolygonCollider2D>();
+//        }
+
+//        Vector2[] points = poly.points;
+//        EdgeCollider2D edge = gameObject.AddComponent<EdgeCollider2D>();
+//        edge.points = points;
+//        Destroy(poly);
+//    }
+//}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,21 +3,52 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // ...
+    [SerializeField] private Transform centerPoint;
+    [SerializeField] private float spawnRadius;
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private LayerMask ballLayer;
+    [SerializeField] private float ballRadius;
+    [SerializeField] private int numOfBalls;
 
     private void Start()
     {
-        Time.timeScale = 0f; // Diðer objelerin hareketini durdurur
-
-        StartCoroutine(StartDelay());
+        SpawnBalls();
+        Time.timeScale = 0f; // Oyunu durdur
     }
 
-    IEnumerator StartDelay()
+    private void SpawnBalls()
     {
-        yield return new WaitForSecondsRealtime(1f); // 1 saniye bekler
-
-        Time.timeScale = 1f; // Diðer objelerin hareketini tekrar baþlatýr
+        for (int i = 0; i < numOfBalls; i++)
+        {
+            Vector2 spawnPoint = GetRandomSpawnPoint();
+            Instantiate(ballPrefab, spawnPoint, Quaternion.identity);
+        }
     }
 
-    // ...
+    private Vector2 GetRandomSpawnPoint()
+    {
+        Vector2 randomPoint = Random.insideUnitCircle.normalized * spawnRadius;
+        Vector2 spawnPoint = new Vector2(centerPoint.position.x, centerPoint.position.y);
+
+        while (Physics2D.OverlapCircle(spawnPoint, ballRadius, ballLayer))
+        {
+            randomPoint = Random.insideUnitCircle.normalized * spawnRadius;
+            spawnPoint = new Vector2(centerPoint.position.x, centerPoint.position.y);
+        }
+
+        return spawnPoint;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Sol týklandý mý kontrol eder
+        {
+            StartGame();
+        }
+    }
+
+    void StartGame()
+    {
+        Time.timeScale = 1f; // Diðer objelerin hareketini baþlatýr
+    }
 }
