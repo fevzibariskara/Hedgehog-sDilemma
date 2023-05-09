efe6b308d3c835c5637661e4947c349205e522f9
Fevzi Barış Kara <58820741+fevzibariskara@users.noreply.github.com>
2023-05-09 06:16:08 +0300

    all is well :33

--- Assets/Scripts/Ball/BallSpawnManager.cs
+++ Assets/Scripts/Ball/BallSpawnManager.cs
@@ -4,48 +4,72 @@ using UnityEngine;
 
 public class BallSpawnManager : MonoBehaviour
 {
-    public GameObject ballPrefab;
-    public int numberOfBalls;
-    //public float spawnRadius;
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] Transform[] spawnPoints;
 
-    [SerializeField] private LayerMask ballLayer = LayerMask.GetMask("Ball");
-
-    [SerializeField] float spawnRadius = 3f;
     private void Start()
     {
-        for (int i = 0; i < numberOfBalls; i++)
+        SpawnBalls();
+    }
+
+    private void SpawnBalls()
+    {
+        foreach (Transform spawnPoint in spawnPoints)
         {
-            SpawnBall();
+            GameObject ball = Instantiate(ballPrefab, spawnPoint.position, Quaternion.identity);
         }
     }
-
-
-    void SpawnBall()
-    {
-        Vector2 spawnPosition = Vector2.zero;
-        do
-        {
-            float angle = Random.Range(0, 2 * Mathf.PI);
-            spawnPosition = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * spawnRadius;
-        } while (Physics2D.OverlapCircle(spawnPosition, 0.5f, ballLayer));
-
-        GameObject ball = Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
-        BallMovement ballMovement = ball.GetComponent<BallMovement>();
-        ballMovement.AddForceAtAngle();
-    }
-
-
-    //private void Awake()
-    //{
-    //    SpawnBalls();
-    //}
-
-    //private void SpawnBalls()
-    //{
-    //    for (int i = 0; i < numberOfBalls; i++)
-    //    {
-    //        Vector2 spawnPosition = Random.insideUnitCircle * spawnRadius;
-    //        Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
-    //    }
-    //}
 }
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class BallSpawnManager : MonoBehaviour
+//{
+//    public GameObject ballPrefab;
+//    public int numberOfBalls;
+//    ////public float spawnRadius;
+
+//    //[SerializeField] private LayerMask ballLayer = LayerMask.GetMask("Ball");
+
+//    [SerializeField] float spawnRadius = 3f;
+//    //private void Start()
+//    //{
+//    //    for (int i = 0; i < numberOfBalls; i++)
+//    //    {
+//    //        SpawnBall();
+//    //    }
+//    //}
+
+
+//    //void SpawnBall()
+//    //{
+//    //    Vector2 spawnPosition = Vector2.zero;
+//    //    do
+//    //    {
+//    //        float angle = Random.Range(0, 2 * Mathf.PI);
+//    //        spawnPosition = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * spawnRadius;
+//    //    } while (Physics2D.OverlapCircle(spawnPosition, 0.5f, ballLayer));
+
+//    //    GameObject ball = Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
+//    //    BallMovement ballMovement = ball.GetComponent<BallMovement>();
+//    //    ballMovement.AddForceAtAngle();
+//    //}
+
+
+//    private void Awake()
+//    {
+//        SpawnBalls();
+//    }
+
+//    private void SpawnBalls()
+//    {
+//        for (int i = 0; i < numberOfBalls; i++)
+//        {
+//            Vector2 spawnPosition = Random.insideUnitCircle * spawnRadius;
+//            Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
+//        }
+//    }
+//}
